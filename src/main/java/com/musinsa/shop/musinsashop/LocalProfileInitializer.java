785a0f8e55a9080d9396f0ca6b1bffef7e56b35b
parker.p <parker.p@kakaoenterprise.com>
2024-05-16 08:12:03 +0900

    test dml 추가

--- src/main/java/com/musinsa/shop/musinsashop/LocalProfileInitializer.java
+++ src/main/java/com/musinsa/shop/musinsashop/LocalProfileInitializer.java
@@ -42,63 +42,44 @@ public class LocalProfileInitializer {
     }
 
     private void initProduct() {
-        initABrandProduct();
-        initBBrandProduct();
+        initBrandProduct("A", new int[] {11200, 5500, 4200, 9000, 2000, 1700, 1800, 2300});
+        initBrandProduct("B", new int[] {10500, 5900, 3800, 9100, 2100, 2000, 2000, 2200});
+        initBrandProduct("C", new int[] {10000, 6200, 3300, 9200, 2200, 1900, 2200, 2100});
+        initBrandProduct("D", new int[] {10100, 5100, 3000, 9500, 2500, 1500, 2400, 2000});
+        initBrandProduct("E", new int[] {10700, 5000, 3800, 9900, 2300, 1800, 2100, 2100});
+        initBrandProduct("F", new int[] {11200, 7200, 4000, 9300, 2100, 1600, 2300, 1900});
+        initBrandProduct("G", new int[] {10500, 5800, 3900, 9000, 2200, 1700, 2100, 2000});
+        initBrandProduct("H", new int[] {10800, 6300, 3100, 9700, 2100, 1600, 2000, 2000});
+        initBrandProduct("I", new int[] {11400, 6700, 3200, 9500, 2400, 1700, 1700, 2400});
     }
 
-    private void initABrandProduct() {
-        ReadBrandDto readBrandDto = brandService.selectBrandByName("A");
+    private void initBrandProduct(String brandName, int[] price) {
+        int priceIdx = 0;
+        ReadBrandDto readBrandDto = brandService.selectBrandByName(brandName);
 
         ReadCategoryDto top = categoryService.selectCategoryByName("상의");
-        productService.createProduct(CreateProductDto.of("A셔츠", 11200, top.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"셔츠", price[priceIdx++], top.id(), readBrandDto.id()));
 
         ReadCategoryDto outer = categoryService.selectCategoryByName("아우터");
-        productService.createProduct(CreateProductDto.of("A아우터", 5500, outer.id(), readBrandDto.id()));
-        
-        ReadCategoryDto bottom = categoryService.selectCategoryByName("바지");
-        productService.createProduct(CreateProductDto.of("A바지", 4200, bottom.id(), readBrandDto.id()));
-
-        ReadCategoryDto shoes = categoryService.selectCategoryByName("스니커즈");
-        productService.createProduct(CreateProductDto.of("A스니커즈", 9000, shoes.id(), readBrandDto.id()));
-
-        ReadCategoryDto bag = categoryService.selectCategoryByName("가방");
-        productService.createProduct(CreateProductDto.of("A가방", 2000, bag.id(), readBrandDto.id()));
-
-        ReadCategoryDto hat = categoryService.selectCategoryByName("모자");
-        productService.createProduct(CreateProductDto.of("A모자", 1700, hat.id(), readBrandDto.id()));
-
-        ReadCategoryDto shocks = categoryService.selectCategoryByName("양말");
-        productService.createProduct(CreateProductDto.of("A양말", 1800, shocks.id(), readBrandDto.id()));
-
-        ReadCategoryDto accessories = categoryService.selectCategoryByName("악세사리");
-        productService.createProduct(CreateProductDto.of("A악세사리", 2300, accessories.id(), readBrandDto.id()));
-    }
-
-    private void initBBrandProduct() {
-        ReadBrandDto readBrandDto = brandService.selectBrandByName("B");
-
-        ReadCategoryDto top = categoryService.selectCategoryByName("상의");
-        productService.createProduct(CreateProductDto.of("B셔츠", 10500, top.id(), readBrandDto.id()));
-
-        ReadCategoryDto outer = categoryService.selectCategoryByName("아우터");
-        productService.createProduct(CreateProductDto.of("B아우터", 5900, outer.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"아우터", price[priceIdx++], outer.id(), readBrandDto.id()));
 
         ReadCategoryDto bottom = categoryService.selectCategoryByName("바지");
-        productService.createProduct(CreateProductDto.of("B바지", 3800, bottom.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"바지", price[priceIdx++], bottom.id(), readBrandDto.id()));
 
         ReadCategoryDto shoes = categoryService.selectCategoryByName("스니커즈");
-        productService.createProduct(CreateProductDto.of("B스니커즈", 9100, shoes.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"스니커즈", price[priceIdx++], shoes.id(), readBrandDto.id()));
 
         ReadCategoryDto bag = categoryService.selectCategoryByName("가방");
-        productService.createProduct(CreateProductDto.of("B가방", 2100, bag.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"가방", price[priceIdx++], bag.id(), readBrandDto.id()));
 
         ReadCategoryDto hat = categoryService.selectCategoryByName("모자");
-        productService.createProduct(CreateProductDto.of("B모자", 2000, hat.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"모자", price[priceIdx++], hat.id(), readBrandDto.id()));
 
         ReadCategoryDto shocks = categoryService.selectCategoryByName("양말");
-        productService.createProduct(CreateProductDto.of("B양말", 2000, shocks.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"양말", price[priceIdx++], shocks.id(), readBrandDto.id()));
 
         ReadCategoryDto accessories = categoryService.selectCategoryByName("악세사리");
-        productService.createProduct(CreateProductDto.of("B악세사리", 2200, accessories.id(), readBrandDto.id()));
+        productService.createProduct(CreateProductDto.of(brandName+"악세사리", price[priceIdx], accessories.id(), readBrandDto.id()));
     }
+
 }
